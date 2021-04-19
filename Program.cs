f6aad8f2e914ccfc3004089c2be4de54e7cdd6e9
Nikolay <kolekdoma@mail.ru>
2021-04-19 07:32:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace On_line_Shop_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
